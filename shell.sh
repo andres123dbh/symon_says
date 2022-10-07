bf067aeeeee34a6dc66a006f4c336b10a34a1087
andres123dbh <60190072+andres123dbh@users.noreply.github.com>
2022-10-06 23:12:39 -0500

    commands

---
+++ shell.sh
@@ -0,0 +1,13 @@
+npm init -y
+npm i typescript
+npx tsc --init
+npm i express mysql ejs
+npm i dotenv
+
+npm i nodemon -D
+npm i @types/express -D
+npm i @types/mysql -D
+npm i @types/ejs -D
+npm i concurrently -D
+
+tsc -w
